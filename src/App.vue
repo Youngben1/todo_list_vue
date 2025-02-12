fc81f7efe8c8965f91e3d7abb0b31639d0769df4
Youngben1 <benjaminrenny@gmail.com>
2025-02-12 14:36:15 +0100

    frontend section completed

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<script setup>
+  import {  ref, computed , onMounted, watch } from "vue"
+
+  const todos = ref([])
+  const name = ref("")
+
+  const input_content = ref("")
+  const input_category = ref(null)
+
+  const todos_asc = computed(() => todos.value.sort((a,b) => {
+    return b.createdAt - a.createdAt
+  }))
+
+  const addTodo = () => {
+    if(input_content.value.trim() === "" || input_category === null) {
+      return
+    }
+
+    todos.value.push({
+      content: input_content.value,
+      category: input_category.value,
+      done: false,
+      date: new Date().getTime()
+    })
+  }
+
+  watch(todos, newVal => {
+    localStorage.setItem("todos", JSON.stringify(newVal))
+  }, {deep: true})
+
+  watch(name, (newVal) => {
+    localStorage.setItem("name", newVal)
+  })
+
+  onMounted(() => {
+    name.value = localStorage.getItem("name") || ""
+  })
+
+</script>
+
+<template>
+  <main class="app">
+
+    <section class="greeting">
+      <h2 class="title">
+        What's Up, <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>
+        CREATE A TODO LIST
+      </h3>
+
+      <form @submit.prevent="addTodo">
+        <h4>What's on your mind to do?</h4>
+        <input type="text" placeholder="E.g: Create an Onlyfans acc" v-model="input_content" />
+        <h4>Pick a Category</h4>
+        <div class="options">
+          <label>
+            <input type="radio" name="category" v-model="input_category" value="business" />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+          <label>
+            <input type="radio" name="category" v-model="input_category" value="personal" />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+
+        <input type="submit" value="Add Todo" />
+      </form>
+    </section>
+
+  </main>
+</template>
+
+
